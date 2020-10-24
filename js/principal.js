1fdf3a243cc8d1b42eb717630b0fd7963a005a94
MininoxD <60321521+MininoxD@users.noreply.github.com>
2020-10-23 19:42:54 -0500

    Funcionamiento del login con Api

--- js/principal.js
+++ js/principal.js
@@ -16,11 +16,30 @@ function cargar_login(){
 
 // Verificar el login para llamar a las paginas
 function login_verifica(){
+
+    //let consulta = "https://gtproyeccion.000webhostapp.com/api/Api_usuarios.php?operacion=validar&dni_u=04079175&contrasena=123";
     var datos = $('#form_login').serialize();
-    var id= $('#operacion').val();
+    datos = datos+"&operacion=validar";
+    $.ajax({
+      url: "https://gtproyeccion.000webhostapp.com/api/Api_usuarios.php",
+      type:"POST",
+      data: datos,
+      async: false,
+      success: function(r, status, XHR){
+            r.map((d)=>{
+              if("Secretaria General" === d.rol){
+                $('#contenedor_principal').empty();
+                cargar_vice();
+              }
+            });
+          
+      },
+      error: function(XHR, status, error){
+        alert("Usuario o Password incorrecto");
+      }
+      });
 
-
-    let usuario = $("#l_usuario").val();
+    /*let usuario = $("#l_usuario").val();
     let pass = $("#l_pass").val();
 
     if(usuario=="admin1" && pass=="admin1"){
@@ -33,7 +52,7 @@ function login_verifica(){
       cargar_vice();
     }else {
       alert("Usuario o Password incorrecto");
-    }
+    }*/
 }
 
 
